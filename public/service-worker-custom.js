27bc6f569afa410b19311253ddc73da02a7f0760
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-11-13 19:24:45 +0500

    feat: Make an empty custom service worker for dev mode

---
+++ public/service-worker-custom.js
@@ -0,0 +1,1 @@
+console.log('--This be the custom service worker transpiring..');
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,7 +21,10 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  console.log('--Let\'s create a service worker.');
+  // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
+    console.log('--We\'re creating a service worker even if it\'s dev mode.');
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
@@ -32,7 +35,8 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker-custom.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
