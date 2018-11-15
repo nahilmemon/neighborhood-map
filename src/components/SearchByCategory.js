a96691393a245d0372818ed01bc5a91311e9323b
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-11-15 17:52:22 +0500

    feat: Add 'none' option to SearchByCategory select

--- src/components/SearchByCategory.js
+++ src/components/SearchByCategory.js
@@ -9,7 +9,12 @@ class SearchByCategory extends Component {
           className="filter-locations-by-category-input"
           name="Filter locations by category:"
           aria-label="Filter locations by category:"
+          value="none"
         >
+          {/* Default option - signifies not to filter by category */}
+          <option value="none">None</option>
+          {/* Get other options dynamically from the
+          this.props.locationCategories array */}
           {this.props.locationCategories.map((category) => {
             return(
               <option
