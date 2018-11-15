f9dbf6d78dd108058c6bd4ecd0bd9a86ca3e1b42
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-11-15 16:35:32 +0500

    feat: Add SearchForm component

---
+++ src/components/SearchForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+  render() {
+    return (
+      <section>
+        <h2 className="side-bar-heading">Filter Locations</h2>
+        <form className="padding-inline-start">
+          <h3 className="side-bar-heading">Filter by Name</h3>
+          <input
+            className="filter-locations-by-name-input"
+            type="text"
+            placeholder="Filter locations by name:"
+            name="Filter locations by name:"
+            aria-label="Filter locations by name:"
+          ></input>
+
+          <h3 className="side-bar-heading">Filter by Category</h3>
+          <select
+            className="filter-locations-by-category-input"
+            name="Filter locations by category:"
+            aria-label="Filter locations by category:"
+          >
+            <option value="parks">Parks</option>
+            <option value="restaurants">Restaurants</option>
+            <option value="shopping">Shopping</option>
+          </select>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default SearchForm;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
 
+import SearchForm from './SearchForm.js';
 import SearchResultsList from './SearchResultsList.js';
 
 class SideBar extends Component {
   render() {
     return (
       <aside className="side-bar-container">
-        <section>
-          <h2 className="side-bar-heading">Filter Locations</h2>
-          <form className="padding-inline-start">
-            <h3 className="side-bar-heading">Filter by Name</h3>
-            <input
-              className="filter-locations-by-name-input"
-              type="text"
-              placeholder="Filter locations by name:"
-              name="Filter locations by name:"
-              aria-label="Filter locations by name:"
-            ></input>
-
-            <h3 className="side-bar-heading">Filter by Category</h3>
-            <select
-              className="filter-locations-by-category-input"
-              name="Filter locations by category:"
-              aria-label="Filter locations by category:"
-            >
-              <option value="parks">Parks</option>
-              <option value="restaurants">Restaurants</option>
-              <option value="shopping">Shopping</option>
-            </select>
-          </form>
-        </section>
+        <SearchForm />
 
         <SearchResultsList
           locationsData={this.props.locationsData}
