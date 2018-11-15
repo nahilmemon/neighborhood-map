4c1017a0f132a520e4e4f2160a58b7162ae0670a
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-11-15 15:12:57 +0500

    style: Change title content based on window width
    Change the contents of the header's title from "AD Hidden Gems" to "Abu Dhabi Hidden Gems" when there's enough space to display the whole title. This was accomplished using CSS in order to avoid unnecessarily re-rendering the page when window width changes.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,10 @@ class Header extends Component {
     return (
       <header className="header">
         <button className="unstyled-button header-button header-search-button">Search</button>
-        <h1 className="site-heading">AD Hidden Gems</h1>
+        <h1
+          className="site-heading"
+          aria-label="Abu Dhabi Hidden Gems"
+        >Hidden Gems</h1>
         <button className="unstyled-button header-button header-about-button button-open-modal">About Project</button>
       </header>
     );
